7a14e726bf85b0be2a3879c705358bd314029636
emarangon <42460615+emarangon@users.noreply.github.com>
2024-03-26 12:17:34 +1000

    Update plutea_physiology.R
    to reflect changes in dataset for publication (i.e. not relevant data removed)

--- plutea_physiology.R
+++ plutea_physiology.R
@@ -15,6 +15,7 @@ library (glmmTMB)  #models
 library (DHARMa) #model assumption
 library(MuMIn)  #r.squaredGLMM
 library(emmeans) #post hocs
+library (tidyr) 
 
 
 
@@ -40,21 +41,20 @@ All <- subset(All, Respiration != "NA" ) %>% droplevels()
 All <- subset(All, Photosynthesis != "NA" )  %>% droplevels()
 All <- subset(All, RatioPR != "NA" )  %>% droplevels()
 summary(All)
-All_noP2 <- subset (All, Parent !="2") %>% droplevels #I remove P2 because it was identified as Porites lobata
-summary(All_noP2)
+
 
 ###data exploration
-ggplot(All_noP2, aes(y=Photosynthesis, x=TimePoint, color=Treatment))+
+ggplot(All, aes(y=Photosynthesis, x=TimePoint, color=Treatment))+
   geom_boxplot()+ 
   theme_classic() +
   geom_point(position=position_dodge(width=0.75),aes(group=Treatment))
 
-ggplot(All_noP2, aes(y=Respiration, x=TimePoint, color=Treatment))+
+ggplot(All, aes(y=Respiration, x=TimePoint, color=Treatment))+
   geom_boxplot()+ 
   theme_classic() +
   geom_point(position=position_dodge(width=0.75),aes(group=Treatment))
 
-ggplot(All_noP2, aes(y=RatioPR, x=TimePoint, color=Treatment))+
+ggplot(All, aes(y=RatioPR, x=TimePoint, color=Treatment))+
   geom_boxplot()+ #points
   theme_classic() +
   geom_point(position=position_dodge(width=0.75),aes(group=Treatment))
@@ -63,7 +63,7 @@ ggplot(All_noP2, aes(y=RatioPR, x=TimePoint, color=Treatment))+
 ###final plots
 
 #Photosynthesis
-tgc <- summarySE(All_noP2, measurevar="Photosynthesis", groupvars=c("Time", "Treatment"))
+tgc <- summarySE(All, measurevar="Photosynthesis", groupvars=c("Time", "Treatment"))
 tgc
 
 ggplot(tgc, aes(Time, Photosynthesis, colour = Treatment)) +
@@ -77,10 +77,10 @@ geom_point (aes(x = Time, y = Photosynthesis, fill=Treatment),
   geom_errorbar(aes(ymin=Photosynthesis-se, ymax=Photosynthesis+se), 
                 width=.4, lwd=.7,  
                 position=position_dodge(.05)) +
-  ggtitle("Porites Photosynthesis") 
+  ggtitle("Porites Net Photosynthesis") 
 
 #Respiration
-tgc <- summarySE(All_noP2, measurevar="Respiration", groupvars=c("Time", "Treatment"))
+tgc <- summarySE(All, measurevar="Respiration", groupvars=c("Time", "Treatment"))
 tgc
 
 ggplot(tgc, aes(Time, Respiration, colour = Treatment)) +
@@ -97,7 +97,7 @@ geom_point (aes(x = Time, y = Respiration, fill=Treatment),
   ggtitle("Porites Respiration") 
 
 #PRratio
-tgc <- summarySE(All_noP2, measurevar="RatioPR", groupvars=c("Time", "Treatment"))
+tgc <- summarySE(All, measurevar="RatioPR", groupvars=c("Time", "Treatment"))
 tgc
 
 ggplot(tgc, aes(Time, RatioPR, colour = Treatment)) +
@@ -111,33 +111,33 @@ geom_point (aes(x = Time, y = RatioPR, fill=Treatment),
   geom_errorbar(aes(ymin=RatioPR-se, ymax=RatioPR+se), 
                 width=.4, lwd=.7,  
                 position=position_dodge(.05)) +
-  ggtitle("Porites RatioPR")
+  ggtitle("Porites P/R ratio")
 
 
 
 ###statistics (final models)
 
 #Photosynthesis
-All_noP2_posP <- All_noP2 %>% dplyr::mutate(pos_photosynthesis = (Photosynthesis+0.009)) #to have all positive values
-All_noP2_posP_sqrt <- All_noP2_posP %>% dplyr::mutate(sqrt_pos_photosynthesis = sqrt(pos_photosynthesis)) #sqrt transformation
+All_posP <- All %>% dplyr::mutate(pos_photosynthesis = (Photosynthesis+0.009)) #to have all positive values
+All_posP_sqrt <- All_posP %>% dplyr::mutate(sqrt_pos_photosynthesis = sqrt(pos_photosynthesis)) #sqrt transformation
 Pmod11 = glmmTMB(sqrt_pos_photosynthesis ~ Parent+TimePoint*Treatment + 
-                  (1|Tank/Sample.ID), data = All_noP2_posP_sqrt, family='gaussian', REML = TRUE)
+                  (1|Tank/Sample.ID), data = All_posP_sqrt, family='gaussian', REML = TRUE)
 P.resid <- Pmod11 %>% simulateResiduals(plot =TRUE) #assumptions met!
 Pmod11 %>% summary () #interaciton sign
 Pmod11 %>% r.squaredGLMM() #our model explains 78% of variability.
 Pmod11 %>% emmeans (~Treatment|TimePoint) %>% pairs %>%rbind(adjust='sidak') 
 
 #Respiration
-All_noP2_abs_respiration <- All_noP2 %>% dplyr::mutate(sqrt_abs_respiration = sqrt(abs_respiration))
+All_abs_respiration <- All %>% dplyr::mutate(abs_respiration = abs(Respiration))
 Rmod7 = glmmTMB(abs_respiration ~ Parent+TimePoint*Treatment + 
-                  (1|Tank/Sample.ID), data = All_noP2_abs_respiration, family='Gamma', REML = TRUE)
+                  (1|Tank/Sample.ID), data = All_abs_respiration, family='Gamma', REML = TRUE)
 R.resid <- Rmod7 %>% simulateResiduals(plot =TRUE)  #assumptions met!
 Rmod7 %>% summary () #interaction time point treatment sign
 Rmod7 %>% emmeans (~Treatment|TimePoint) %>% pairs %>%rbind(adjust='sidak')
 
 #PR ratio
 PRmod5 = glmmTMB(RatioPR ~ Parent+Treatment*TimePoint + 
-                  (1|Tank/Sample.ID), data = All_noP2, family='Gamma', REML = TRUE)
+                  (1|Tank/Sample.ID), data = All, family='Gamma', REML = TRUE)
 PR.resid <- PRmod5 %>% simulateResiduals(plot =TRUE) #assumptions met!
 PRmod5 %>% summary () #Treatment*TimePoint sign
 PRmod5 %>% emmeans (~Treatment|TimePoint) %>% pairs %>%rbind(adjust='sidak')
@@ -164,18 +164,17 @@ summary(All)
 class(All$Time)
 class(All$temp_celsius)
 class(All$Tank)
-All_noP2 <- subset (All, Parent !="P2") %>% droplevels #I remove P2 because it was identified as Porites lobata
-All_noP2_noT5a <- subset (All_noP2, TimePoint !="T5a") %>% droplevels #filter data (remove extra time point)
+
 
 ###data exploration
-ggplot(All_noP2_noT5a, aes(y=BleachigScore, x=TimePoint, color=Treatment))+
+ggplot(All, aes(y=HealthScore, x=TimePoint, color=Treatment))+
   geom_boxplot()+ 
   theme_classic() +
   geom_point(position=position_dodge(width=0.75),aes(group=Treatment))
 
 
 ###final plot
-tgc <- summarySE(All_noP2_noT5a, measurevar="BleachigScore", groupvars=c("TimePoint", "Treatment"))
+tgc <- summarySE(All, measurevar="HealthScore", groupvars=c("TimePoint", "Treatment"))
 tgc
 
 tgc2 <- tgc %>% #add a Time column
@@ -186,29 +185,29 @@ tgc2 <- tgc %>% #add a Time column
       TimePoint == "T2" ~ "27",
       TimePoint == "T3" ~ "34",
       TimePoint == "T4" ~ "41",
-      TimePoint == "T5b" ~ "56"
+      TimePoint == "T5" ~ "56"
     )
   )
 tgc2$Time <- as.numeric(tgc2$Time)
 
-ggplot(tgc2, aes(Time, BleachigScore, colour = Treatment)) +
+ggplot(tgc2, aes(Time, HealthScore, colour = Treatment)) +
   geom_line() +
   scale_x_continuous(breaks=c(0, 19, 27, 34, 41, 56)) + 
   scale_y_continuous(breaks = scales::pretty_breaks(n = 8)) +
     theme_classic() + geom_line(size=0.7)+ 
 scale_color_manual(values=c("#B4B4B4", "#AC664B")) + 
-geom_point (aes(x = Time, y = BleachigScore, fill=Treatment),
+geom_point (aes(x = Time, y = HealthScore, fill=Treatment),
                         shape=21) + #to include points on lines
   scale_fill_manual(values=c("#B4B4B4", "#AC664B")) + 
-  geom_errorbar(aes(ymin=BleachigScore-se, ymax=BleachigScore+se), 
+  geom_errorbar(aes(ymin=HealthScore-se, ymax=HealthScore+se), 
                 width=.4, lwd=.7,  
                 position=position_dodge(.05)) +
-  ggtitle("Porites Bleaching score")
+  ggtitle("Porites Health score")
 
 
 ###statistics (final model)
-Bmod5 = glmmTMB(BleachigScore ~ Parent+TimePoint*Treatment + 
-                  (1|Tank/Sample.ID), data = All_noP2_noT5a, family='gaussian', REML = TRUE)
+Bmod5 = glmmTMB(HealthScore ~ Parent+TimePoint*Treatment + 
+                  (1|Tank/Sample.ID), data = All, family='gaussian', REML = TRUE)
 B.resid <- Bmod5 %>% simulateResiduals(plot =TRUE) #assumptions met!
 Bmod5 %>% summary ()
 Bmod5 %>% r.squaredGLMM() #our model explains 55% of variability
@@ -221,9 +220,9 @@ Bmod5 %>% emmeans (~Treatment|TimePoint) %>% pairs %>%rbind(adjust='sidak')
 ############################################################################
 
 ###preprocessing
-Light_1_filtered_sd <- read.csv("./Light_porites_filteringSD.csv", header = TRUE, sep=",")
+Light_1_filtered_sd <- read.csv("./PhotochemicalEfficiency_porites.csv", header = TRUE, sep=",")
 Light_2_filtered_sd <- unite(Light_1_filtered_sd, ID2, TimePoint:Light_Dark, remove=FALSE)
-Light_2_filtered_sd$ID2 <- as.factor(Light_2_filtered_sd$ID2)
+Light_2_filtered_sd$ID2 <- as.factor(Light_2_filtered_sd$ID2) 
 
 Light_3_filtered_sd <- unite(Light_2_filtered_sd, ID3, ID2:Sample_ID, remove=FALSE)
 Light_3_filtered_sd$ID3 <- as.factor(Light_3_filtered_sd$ID3)
@@ -257,11 +256,10 @@ Light_filtered_sd <- Light_8_filtered_sd %>%
          Sample_ID = factor(Sample_ID))
 summary(Light_filtered_sd)
 
-Light_filtered_sd_noP2 <- subset(Light_filtered_sd, Parent != "P2" & TimePoint !="T5a") %>% droplevels() #filtering out final data
 
 
 ###final plot
-Light_filtered_sd_noP2_graph <- Light_filtered_sd_noP2 %>% 
+Light_filtered_sd_graph <- Light_filtered_sd %>% 
   mutate(
     Time = case_when(
       TimePoint == "T0" ~ "0",
@@ -269,11 +267,11 @@ Light_filtered_sd_noP2_graph <- Light_filtered_sd_noP2 %>%
       TimePoint == "T2" ~ "27",
       TimePoint == "T3" ~ "34",
       TimePoint == "T4" ~ "41",
-      TimePoint == "T5b" ~ "56"
+      TimePoint == "T5" ~ "56"
     )
   )
 
-tgc <- summarySE(Light_filtered_sd_noP2_graph, measurevar="PhotochemicalEfficiency", groupvars=c("Time", "Treatment"))
+tgc <- summarySE(Light_filtered_sd_graph, measurevar="PhotochemicalEfficiency", groupvars=c("Time", "Treatment"))
 tgc
 
 tgc$Time = as.numeric(tgc$Time) #need to use this variabe as numeric if I want to add geom_line (lines) to graph
@@ -293,9 +291,9 @@ ggplot(tgc, aes(Time, PhotochemicalEfficiency, colour = Treatment)) +
 
 
 ###statistics (final model)
-Light_filtered_sd_noP2_sqrt <- Light_filtered_sd_noP2 %>% dplyr::mutate(sqrt_efficiency = sqrt(PhotochemicalEfficiency))
+Light_filtered_sd_sqrt <- Light_filtered_sd %>% dplyr::mutate(sqrt_efficiency = sqrt(PhotochemicalEfficiency))
 Emod6 = glmmTMB(sqrt_efficiency ~ Parent+Treatment*TimePoint + 
-                  (1|Tank/Sample_ID), data = Light_filtered_sd_noP2_sqrt, beta_family (link='logit'), REML = TRUE)
+                  (1|Tank/Sample_ID), data = Light_filtered_sd_sqrt, beta_family (link='logit'), REML = TRUE)
 
 B.resid <- Emod6 %>% simulateResiduals(plot=TRUE, integerResponse = TRUE) #acceptable
 Emod6 %>% summary ()
